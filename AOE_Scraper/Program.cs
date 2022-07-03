86c51f685ce089fab7070e58ba4da8e8ab6b513e
Rodrigo DAmbrosio <rodrigo.dambrosio@gmail.com>
2022-07-03 11:54:08 -0300

    Add project files.

---
+++ AOE_Scraper/Program.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+
+List<string> titulos = new();
+HtmlWeb web = new HtmlWeb();
+HtmlDocument doc = web.Load("https://www.ageofempires.com/news/category/releases?game=aoeii");
+
+foreach (var titulo in doc.DocumentNode.CssSelect(".post__title"))
+{
+    if (titulo.InnerHtml.Contains("Update") || titulo.InnerHtml.Contains("Event"))
+        titulos.Add(titulo.InnerHtml);
+}
+// Add aoe3 web
+// Make config file and telegram bot
+// Send telegram msg
